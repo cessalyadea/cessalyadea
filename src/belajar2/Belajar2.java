a94c1275c95a78854b4390e7a1198a4cf805237b
LENOVO <LENOVO@LAPTOP-5DMSOMGE>
2021-07-30 09:23:02 +0700

    https://github.com/cessalyadea/cessalyadea.git

---
+++ src/belajar2/Belajar2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package belajar2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Belajar2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int a, b;
+        for (a = 0; a < 5; a++) { 
+           for(int i=0; i<a; i++){
+               System.out.print("*");
+            }
+           System.out.println(" ");
+        }
+    }
+    
+}
